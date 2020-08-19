a017827b4cc3ba062e6a172a47221225932f631e
nickfil <nikolisfilip@gmail.com>
2020-08-19 20:46:37 +0300

    server files for heroku

---
+++ server/build/precache-manifest.3161bf1626f332aef4bda7503eec00e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c500e016541f330daa3746c88e799ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc72f162965c71268e0e",
+    "url": "/static/css/main.6b3eabc3.chunk.css"
+  },
+  {
+    "revision": "43b43bb8796b4d9df9ec",
+    "url": "/static/js/2.2021d07f.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/static/js/2.2021d07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc72f162965c71268e0e",
+    "url": "/static/js/main.76753cc0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d933d51000e83b03357b18e0dc025379",
+    "url": "/static/media/ophelos_logo.d933d510.png"
+  }
+]);
