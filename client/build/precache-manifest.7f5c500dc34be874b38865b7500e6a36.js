131bfa7ea8f663330fec89957f6f126898cf0a3b
nickfil <nikolisfilip@gmail.com>
2020-08-19 17:22:22 +0300

    add build

---
+++ client/build/precache-manifest.7f5c500dc34be874b38865b7500e6a36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa1a1fc05285e6b66ca93cd884cc20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9647c736506ae9498f5",
+    "url": "/static/css/main.6b3eabc3.chunk.css"
+  },
+  {
+    "revision": "43b43bb8796b4d9df9ec",
+    "url": "/static/js/2.2021d07f.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/static/js/2.2021d07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9647c736506ae9498f5",
+    "url": "/static/js/main.8923f0da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d933d51000e83b03357b18e0dc025379",
+    "url": "/static/media/ophelos_logo.d933d510.png"
+  }
+]);
